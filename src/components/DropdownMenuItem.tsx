39f4c4efae6563a518db3ca3214c44f4c501ffba
Eunmi Lee <eunoia0235@gmail.com>
2026-06-23 18:12:49 +0900

    feat: 드랍다운 메뉴에 서브텍스트 추가

--- src/components/DropdownMenuItem.tsx
+++ src/components/DropdownMenuItem.tsx
@@ -1,17 +1,26 @@
 type Props = {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   text: string;
+  subText?: string;
   onClick: () => void;
 };
 
-export default function DropdownMenuItem({ icon, text, onClick }: Props) {
+export default function DropdownMenuItem({
+  icon,
+  text,
+  subText,
+  onClick,
+}: Props) {
   return (
     <button
       onClick={onClick}
       className="flex items-center gap-3 w-full px-4 py-3 text-sm hover:bg-white/10 cursor-pointer"
     >
-      <span className="text-lg">{icon}</span>
-      {text}
+      {icon && <span className="text-lg">{icon}</span>}
+      <div className="flex flex-col items-start">
+        <div>{text}</div>
+        <div className="text-zinc-400 text-xs">{subText}</div>
+      </div>
     </button>
   );
 }
