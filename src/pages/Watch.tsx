8fb52f66aca636a0193076928457d309d894dbe0
Eunmi Lee <eunoia0235@gmail.com>
2026-06-30 18:11:38 +0900

    feat: 댓글 섹션 컴포넌트 추가

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,10 +1,17 @@
 import { useSearchParams } from "react-router";
 import VideoPlayer from "../components/VideoPlayer";
+import Comment from "../components/Comment";
 
 export default function Watch() {
   const [searchParams] = useSearchParams();
   const videoId = searchParams.get("v");
+
   if (!videoId) return <div>영상을 선택해주세요</div>;
 
-  return <VideoPlayer id={videoId} />;
+  return (
+    <div className="mx-4">
+      <VideoPlayer id={videoId} />
+      <Comment id={videoId} />
+    </div>
+  );
 }
