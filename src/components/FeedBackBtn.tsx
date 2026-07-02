ef2ec3a79664dedcca85f2028f24cba1d1a096d1
Eunmi Lee <eunoia0235@gmail.com>
2026-07-02 16:37:15 +0900

    feat: disabled props 추가로 인한 스타일 추가

--- src/components/FeedBackBtn.tsx
+++ src/components/FeedBackBtn.tsx
@@ -5,6 +5,7 @@ interface Props {
   style?: string;
   iconSize?: string;
   textSize?: string;
+  disabled?: boolean;
 }
 
 export default function FeedBackBtn({
@@ -14,11 +15,14 @@ export default function FeedBackBtn({
   style,
   iconSize,
   textSize,
+  disabled,
 }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`flex items-center justify-center gap-2 px-4 h-9 font-semibold rounded-full hover:bg-zinc-100/20 cursor-pointer ${style ?? "bg-zinc-800"}`}
+      className={`flex items-center justify-center gap-2 px-4 h-9 font-semibold rounded-full hover:bg-zinc-100/20 cursor-pointer disabled:hover:bg-zinc-800 disabled:opacity-40 disabled:cursor-not-allowed
+      } ${style ?? "bg-zinc-800"}`}
+      disabled={disabled}
     >
       {icon && <span className={`${iconSize || "text-xl"}`}>{icon}</span>}
       {text && (
