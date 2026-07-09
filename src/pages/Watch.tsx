26e567eee4964d6908ae0058ed54884bcc157e76
Eunmi Lee <eunoia0235@gmail.com>
2026-07-09 18:18:08 +0900

    feat: comment 컴포넌트 스타일을 바깥에서 주는 걸로 변경

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -8,6 +8,7 @@ export default function Watch() {
   const videoId = searchParams.get("v");
   const location = useLocation();
   const keyword = (location.state as { keyword?: string })?.keyword;
+  console.log(videoId);
 
   if (!videoId) return <div>영상을 선택해주세요</div>;
 
@@ -17,7 +18,9 @@ export default function Watch() {
         {/* row일 땐 VideoPlayer+Comment 묶음, col일 땐 VideoPlayer만 먼저 */}
         <div className="order-1 lg:order-none lg:flex-[2] lg:min-w-0">
           <VideoPlayer id={videoId} />
-          <Comment id={videoId} className="hidden lg:block" />
+          <div className="hidden lg:block">
+            <Comment id={videoId} />
+          </div>
         </div>
 
         <div className="order-2 lg:order-none lg:flex-1 lg:min-w-[320px] lg:shrink-[0.3]">
