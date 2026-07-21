3e80a43e7c4d46363bbbd57b77fbe16060696607
Eunmi Lee <eunoia0235@gmail.com>
2026-07-21 15:22:40 +0900

    feat: 실제 유튜브 데이터로 변경

--- src/context/youtubeApiContext.tsx
+++ src/context/youtubeApiContext.tsx
@@ -1,9 +1,7 @@
 import { createContext } from "react";
 import Youtube from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
 
-const youtube: Youtube = new FakeYoutube(); // new Youtube();
-
+const youtube: Youtube = new Youtube();
 export const YoutubeApiContext = createContext<{
   youtube: Youtube;
 } | null>({
